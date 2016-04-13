489af7be575438323494662dd5dfe5194d3172ae
zack <zack.bitcoin@gmail.com>
2016-04-13 10:04:13 -0700

    change html to look nicer

--- src/constants.erl
+++ src/constants.erl
@@ -2,7 +2,7 @@
 -compile(export_all).
 port() -> 4040.
 gnu_port() -> 4041.
-wait_on_gnu() -> 120000.%2 minutes
+wait_on_gnu() -> 120000.%if the AI takes more than 2 minutes to think, their move wont get updated, and you have to click the "refresh" button eventually.
 difficulty() -> 17.
 ram() -> 500.%in megabytes
     
